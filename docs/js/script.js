3951f5be9bfe560d9abcc0ed2878af7238c19b6e
Usuario <usuario@MacBook-Pro-de-Usuario.local>
2020-06-16 23:47:51 -0300

    pagina totalmente maquetada

--- docs/js/script.js
+++ docs/js/script.js
@@ -4,7 +4,7 @@ window.addEventListener('load',()=>{
     let a = barraNav.children[0]
     window.onscroll = function() {
         
-        if(window.scrollY > 200){
+        if(window.scrollY > 50){
             nav.classList.add('scroleo')
         }else{
             nav.classList.remove('scroleo')
